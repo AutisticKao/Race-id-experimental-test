b66b70e71dc57585f4d8e19b32c551440613d9bc
Ghifarel Ahnaf <muhammad.hariadi@binus.ac.id>
2024-11-07 15:53:14 +0700

    updates

--- race-details.php
+++ race-details.php
@@ -1,5 +1,11 @@
 <?php include 'header.php'; ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salonpas Run 2024</title>
 
 
         <div class="race-details fade-in">
